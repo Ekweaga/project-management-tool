045ada237b2646199e1c9045503988a613fe4404
Ekweaga <hikay133@gmail.com>
2022-10-13 16:55:09 +0100

    Media query

--- pages/Components/Navbar.jsx
+++ pages/Components/Navbar.jsx
@@ -5,13 +5,13 @@ import Link from 'next/link'
 function Navbar() {
   return (
     <>
-    <nav className='text-white bg-[#043873] h-[80px] flex justify-between items-center px-[30px]'>
+    <nav className='text-white bg-[#043873] h-[80px] flex justify-between items-center px-[30px] '>
         <div>
             <h1 className='font-bold text-2xl'>
                 WebSpace
             </h1>
         </div>
-        <ul className='flex items-center justify-center gap-[30px]'>
+        <ul className='md:flex items-center justify-center gap-[30px] hidden'>
             <li>Home</li>
             <li>Our Solutions</li>
             <li>Get Started</li>
--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
       </Head>
 
       <header className='bg-[#043873] h-[500px] text-white'>
-        <div className="flex items-center justify-around px-[20px]">
+        <div className="flex items-center justify-around px-[20px] flex-col md:flex-row">
 
-        <div className="flex flex-col w-[600px]">
+        <div className="flex flex-col md:w-[600px]">
           <h1 className='text-5xl font-bold mb-[20px] leading-[60px]'>Get More Done with webSpace</h1>
           <p>Project Management tool that enable your team to collaborate,plan,analyze and manage everyday tasks</p>
             <div><button className='w-[200px] p-2 bg-[#FFE492] mt-[50px] text-black'>GET STARTED</button></div>
@@ -30,8 +30,8 @@ export default function Home() {
 
 
       <div className='OurTool mt-[80px]'>
-        <div className='OurToolContainer flex items-center justify-center px-[30px]'>
-          <div className="flex flex-col w-[600px]">
+        <div className='OurToolContainer flex items-center justify-center px-[30px] flex-col md:flex-row'>
+          <div className="flex flex-col md:w-[600px]">
             <h1 className="text-5xl font-bold ">Project <br/>Management</h1>
             <span><Image src='/Element.png' width="300px" height="20px" alt="element"></Image></span>
             <div className='mt-[20px]'>
@@ -48,12 +48,12 @@ export default function Home() {
 
       <div className="mt-[70px]">
 
-      <div className='OurToolContainer flex items-center justify-between px-[30px] gap-[30px]'>
+      <div className='OurToolContainer flex items-center justify-between px-[30px] gap-[30px]  flex-col-reverse md:flex-row'>
 
       <div>
           <Image src="/toget.png" alt="coworker" width="500px" height="500px"></Image>
           </div>
-          <div className="flex flex-col w-[600px]">
+          <div className="flex flex-col md:w-[600px]">
             <h1 className="text-5xl font-bold ">Work Together</h1>
             <span><Image src='/Elements.png' width="300px" height="20px" alt="element"></Image></span>
             <div className='mt-[20px]'>
@@ -68,9 +68,9 @@ export default function Home() {
 
 
  
-      <div className='OurTool mt-[80px] bg-[#043873] text-white'>
-        <div className='OurToolContainer flex items-center justify-center px-[30px]'>
-          <div className="flex flex-col w-[600px]">
+      <div className='OurTool mt-[80px] bg-[#043873] text-white py-4'>
+        <div className='OurToolContainer flex items-center justify-center px-[30px] flex-col-reverse md:flex-row'>
+          <div className="flex flex-col md:w-[600px]">
             <h1 className="text-5xl font-bold ">Connect with <br/> other workers</h1>
             <span><Image src='/Element.png' width="300px" height="20px" alt="element"></Image></span>
             <div className='mt-[20px]'>
@@ -88,12 +88,12 @@ export default function Home() {
 
       <div className="mt-[70px]">
 
-<div className='OurToolContainer flex items-center justify-between px-[30px] gap-[30px]'>
+<div className='OurToolContainer flex items-center justify-between px-[30px] gap-[30px] flex-col md:flex-row'>
 
 <div>
     <Image src="/anal.svg" alt="coworker" width="500px" height="500px"></Image>
     </div>
-    <div className="flex flex-col w-[600px]">
+    <div className="flex flex-col md:w-[600px]">
       <h1 className="text-5xl font-bold ">Easy Communication</h1>
       <span><Image src='/Elements.png' width="300px" height="20px" alt="element"></Image></span>
       <div className='mt-[20px]'>
@@ -113,7 +113,7 @@ export default function Home() {
     Your work, everywhere you are
   </h1>
   <span className="-mt-[20px] ml-[250px]"><Image src='/tirbe.png' width="170px" height="10px" alt="element"></Image></span>
-  <p className="w-[700px] text-center">Access your projects from your computer,phone or tablet anywhere you go.This app allows you to work with other workers on same projects,giving updates and notifications on latest tasks to be done</p>
+  <p className="md:w-[700px] text-center">Access your projects from your computer,phone or tablet anywhere you go.This app allows you to work with other workers on same projects,giving updates and notifications on latest tasks to be done</p>
   <div><button className='w-[150px] p-2 bg-[#4F9CF9] mt-[50px] text-white shadow' style={{borderRadius:'10px'}}>Try webspace</button></div>
 </div>
 <div className='absolute -top-[80px] right-0 z-1 px-3'>
@@ -126,8 +126,8 @@ export default function Home() {
 
 
 <div className='OurTool mt-[80px]'>
-        <div className='OurToolContainer flex items-center justify-center px-[30px]'>
-          <div className="flex flex-col w-[600px]">
+        <div className='OurToolContainer flex items-center justify-center px-[30px] flex-col md:flex-row'>
+          <div className="flex flex-col md:w-[600px]">
             <h1 className="text-5xl font-bold ">100% your data</h1>
             <span><Image src='/Element.png' width="300px" height="20px" alt="element"></Image></span>
             <div className='mt-[20px]'>
@@ -147,7 +147,7 @@ export default function Home() {
 <h1 className="text-5xl font-bold ">Our Sponsors</h1>
             <span><Image src='/Element.png' width="150px" height="20px" alt="element"></Image></span>
 </div>
-    <div className="w-[60%] mx-auto flex items-center justify-between mt-[30px]">
+    <div className="w-[80%] mx-auto grid grid-cols-2 md:grid-cols-4 items-center justify-between mt-[30px] gap-[30px]">
     <Image src="/apple.png" alt="coworker" width="80px" height="50px"></Image>
     <Image src="/Google.png" alt="coworker" width="100px" height="50px"></Image>
     <Image src="/Microsoft.png" alt="coworker" width="100px" height="50px"></Image>
@@ -163,12 +163,12 @@ export default function Home() {
 
 
 
-  <div className='OurToolContainer flex items-center justify-around px-[30px] gap-[30px] bg-[#043873] p-6 text-white'>
+  <div className='OurToolContainer flex items-center justify-around px-[30px] gap-[30px] bg-[#043873] p-6 text-white flex-col md:flex-row'>
 
 <div>
     <Image src="/Apps.png" alt="coworker" width="300px" height="300px"></Image>
     </div>
-    <div className="flex flex-col w-[600px]">
+    <div className="flex flex-col md:w-[600px]">
       <h1 className="text-5xl font-bold ">Work with your favourite Apps using Webspace</h1>
       <span><Image src='/Elements.png' width="300px" height="20px" alt="element"></Image></span>
       <div className='mt-[20px]'>
@@ -185,7 +185,7 @@ export default function Home() {
       <h1 className='text-center text-2xl font-bold'>What Our Clients Says</h1>
     </div>
 
-    <div className='flex items-center justify-center gap-[40px] TestimonialsContainer'>
+    <div className='flex items-center justify-center gap-[40px] TestimonialsContainer flex-col md:flex-row'>
       <div className='client h-[300px] w-[300px] shadow flex flex-col p-3 py-[15px]' style={{boxShadow:'-1px 100px 100px rgba(0,0,0,0.1)',borderRadius:'15px'}}>
         <div>
         <Image src='/Quotes.png' width="50px" height="30px" alt="element"></Image>
